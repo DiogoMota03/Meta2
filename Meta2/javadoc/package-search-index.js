2af8f4fc0428efa76a084b60857cc6586cb51d94
Diogo Mota <diogomotauni@gmail.com>
2024-05-17 17:06:49 +0100

    javadoc

---
+++ Meta2/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"googol.barrel"},{"l":"googol.client"},{"l":"googol.downloader"},{"l":"googol.gateway"},{"l":"googol.queue"},{"l":"org.example.meta2"}];updateSearchResults();
